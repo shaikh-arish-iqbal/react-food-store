523a1f838032141adac3981e82fc75df5998a199
Shaikh Arish Iqbal <shaikharishiqbal@gmail.com>
2025-01-15 21:28:09 +0530

    publish on render

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 dotenv.config();
-// import { fileURLToPath } from 'url';
+import { fileURLToPath } from 'url';
 import express from 'express';
 import cors from 'cors';
 import foodRouter from './routers/food.router.js';
@@ -9,11 +9,11 @@ import userRouter from './routers/user.router.js';
 // import uploadRouter from './routers/upload.router.js';
 
 import { dbconnect } from './config/database.config.js';
-// import path, { dirname } from 'path';
+import path, { dirname } from 'path';
 dbconnect();
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 app.use(express.json());
@@ -29,20 +29,20 @@ app.use('/api/users', userRouter);
 // app.use('/api/orders', orderRouter);
 // app.use('/api/upload', uploadRouter);
 
-// const publicFolder = path.join(__dirname, 'public');
-// app.use(express.static(publicFolder));
+const publicFolder = path.join(__dirname, 'public');
+app.use(express.static(publicFolder));
 
-// app.get('*', (req, res) => {
-//   const indexFilePath = path.join(publicFolder, 'index.html');
-//   res.sendFile(indexFilePath);
-// });
-
-const PORT = 5000;
-app.listen(PORT, () => {
-  console.log('listening on port ' + PORT);
+app.get('*', (req, res) => {
+  const indexFilePath = path.join(publicFolder, 'index.html');
+  res.sendFile(indexFilePath);
 });
 
-// const PORT = process.env.PORT || 5000;
+// const PORT = 5000;
 // app.listen(PORT, () => {
 //   console.log('listening on port ' + PORT);
 // });
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log('listening on port ' + PORT);
+});
